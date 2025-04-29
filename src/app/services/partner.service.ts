54d1409e0e2a0f665a62f8953855cdd911ea6840
Rusu Bogdan <bogdanrusu98@yahoo.ro>
2025-04-29 20:46:57 +0300

    update

--- src/app/services/partner.service.ts
+++ src/app/services/partner.service.ts
@@ -3,16 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, map, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Partner } from '../models/partner.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class PartnerService {
-  private apiUrl = 'https://mockanapi.com/s/67ae1b3403f9ffca6f47eb79/partners?mock_delay=5000';
 
-  constructor(private http: HttpClient) {}
+  private apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
 
   getPartners(): Observable<Partner[]> {
     return this.http.get<{ [key: string]: Partner }>(this.apiUrl).pipe(
-      map(data => Object.values(data)), 
+      map(data => Object.values(data)),
       catchError(error => {
         console.error('API error:', error);
         return throwError(() => new Error('Failed to load partner data'));
